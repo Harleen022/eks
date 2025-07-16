68a26f095a2cee83fc76b6285bee30fa0f97cbf2
root <root@openstack.homelab.xyz>
2025-07-16 08:10:17 +0000

    update the branch name

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-dev"  # Change this to match your bucket name
+    bucket         = "githubackendcicd-s3"  # Change this to match your bucket name
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"  # Updated region
     encrypt        = true

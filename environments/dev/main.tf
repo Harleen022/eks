ed25fd42c9ef8fe6c75ad27b2b0c585c76f6bac6
root <root@openstack.homelab.xyz>
2025-07-16 08:21:11 +0000

    update the env region

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"  # Updated region
+  region = "us-east-1"  # N. Virginia region
 }
 
 module "terraform_state_bucket" {
   source = "../../modules/s3"
-  bucket_name = "my-terraform-state-bucket-dev"  # Change this to your desired bucket name
+  bucket_name = "githubackendcicd-s3"
 } 
